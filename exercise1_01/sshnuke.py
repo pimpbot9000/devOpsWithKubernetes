4ae67cae024c10663479d3e0ce31469c50d40538
Tuomas Välimäki <t.valimaki@gmail.com>
2021-03-10 20:03:18 +0200

    Cleanup dumb shit

--- exercise1_01/sshnuke.py
+++ exercise1_01/sshnuke.py
@@ -12,10 +12,9 @@ class App():
     def start_timer(self):
         while True:
             dt = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-            output_str = 'echo {dt}: {random_str}'.format(dt=dt, random_str=self.random_str)
-            #print(output_str)
+            output_str = 'echo {dt}: {random_str}'.format(dt=dt, random_str=self.random_str)            
             os.system(output_str)
-            time.sleep(5)
+            time.sleep(self.delay)
 
 if __name__ == "__main__":
     app = App(N=10, delay=5)
