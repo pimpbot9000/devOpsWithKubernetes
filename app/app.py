0519cb30f3a82f45cfb9b82fbc0a5b238485ce74
pimpbot9000 <t.valimaki@gmail.com>
2021-03-11 15:28:44 +0200

    Simple app that gives a hash as a reponse. Default port 8080

--- app/app.py
+++ app/app.py
@@ -1,20 +1,34 @@
 from flask import Flask, jsonify, request, Response, abort
+import hashlib
+import time
+import sys
 
+# global hash
+hs = ""
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def root():
-    arg = request.args.get('name')
+    #arg = request.args.get('name')
+    response_hash = hashlib.sha256(str(time.time()).encode('utf-8')).hexdigest()[0:10]
+    
+    response_str = 'Application {hs}. Response {response_hash}'.format(hs=hs, response_hash=response_hash)
 
-    if arg is not None:
-        return Response("hello " + arg, status=200)
-    else:
-        return Response("hello anon!", status=200)
+    return Response(response_str, status=200)
+   
 
 if __name__ == "__main__":
     from waitress import serve
-    host = "0.0.0.0"
+
     port = 8080
+
+    try:
+        port = sys.argv[1:][0]
+    except:
+        pass
+    
+    host = "0.0.0.0"    
+    hs = hashlib.sha256(str(time.time()).encode('utf-8')).hexdigest()[0:10]
     print('Server started in port {port}'.format(port=port))    
     serve(app, host=host, port=port)
     
