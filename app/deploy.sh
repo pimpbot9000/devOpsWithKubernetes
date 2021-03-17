21831d12737baabef0ee3d64944efce960fa10bf
pimpbot9000 <t.valimaki@gmail.com>
2021-03-17 14:23:40 +0200

    exercise_109: moar services

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,3 +1,3 @@
-k3d cluster create --port '8082:30080@agent[0]' --port '8081:80@loadbalancer' --agents 5
+k3d cluster create --port '8082:30080@agent[0]' --port '8081:80@loadbalancer' --agents 3
 kubectl apply -f manifests/deployment.yaml
-kubectl apply -f manifests/service.yaml
+kubectl apply -f manifests/service-NodePort.yaml
