cc6acd30760a3a97b3a1f1100d8083fe96d045a7
pimpbot9000 <t.valimaki@gmail.com>
2021-03-19 16:10:24 +0200

    Exercise 1.11

---
+++ exercise1_11/deploy.sh
@@ -0,0 +1,15 @@
+docker build -t pimpbot9000/app2:latest -f Dockerfile.app2 .
+docker push pimpbot9000/app2:latest
+
+docker build -t pimpbot9000/pong2:latest -f Dockerfile.pong2 .
+docker push pimpbot9000/pong2:latest
+
+k3d cluster create --port '1337:80@loadbalancer' --agents 2
+docker exec k3d-k3s-default-agent-0 mkdir -p /tmp/kube
+
+kubectl apply -f manifests/persistent-volume.yaml
+kubectl apply -f manifests/persistent-claim.yaml
+kubectl apply -f manifests/deployment.yaml
+kubectl apply -f manifests/ingress.yaml
+kubectl apply -f manifests/service-app2.yaml
+kubectl apply -f manifests/service-pong2.yaml
